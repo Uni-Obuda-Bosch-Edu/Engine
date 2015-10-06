e796a8e0a278a70dbe303527731b740703f68149
Roland Halbaksz <redcoreit@gmail.com>
2015-10-06 04:45:34 -0700

    Cleaning the repo

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Reptile on 2015.09.30..
- */
-public class Test {
-    public static void main(String[] args)
-    {
-        Engine testEngine = new Engine();
-    }
-}
--- src/busInterface/Gearbox_Out.java
+++
@@ -1,7 +0,0 @@
-package busInterface;
-
-public interface Gearbox_Out {
-	public void setGearTorque(int gearTorque);
-	public void setGearRevolution(int gearRevolution);
-	public void setGearMode(int gearMode);
-}
